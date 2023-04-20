0567c598d62e858777a16053afbdb0a13d5c4d43
RoberRze <robert_rzeszotek@sevenet.pl>
2023-04-20 19:27:22 +0200

    Add zmienne.py, bool.py scripts

---
+++ 00_intro/bool.py
@@ -0,0 +1,15 @@
+eq = 23 + 3 == 15 + 12
+print('1.', eq)
+eq2=29//7 == 5
+print('2.',eq2)
+eq3= 27%8 == 3
+print('3.',eq3)
+num = int(input('Podaj liczbe: '))
+eq4 = num % 2 ==0
+print('4.',eq4)
+eq5 = 43-13 == 11+12
+print('5.',eq5)
+eq6 = 129 // 17 == 3
+print('6.',eq6)
+eq7 = 247 % 5 == 2
+print('7.',eq7)
