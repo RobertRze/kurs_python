97c41bf66ba5e27d9ad58fa714cbd5db89a7ee6e
RobertRze <robert_rzeszotek@sevenet.pl>
2023-05-01 22:19:04 +0200

    list taks 2, 4

--- 03/list.py
+++ 03/list.py
@@ -2,25 +2,35 @@
 
 
 arr = [ 3, 5, 7, 2, 4, 2, 6, 2]
+arr.append(4)
+arr.extend([1, 2, 3])
+arr.insert(1, 100)
+arr.remove(7)
+print(arr.index(100))
 print(arr.count(2))
-
-
-#kopiowanie listy
-import copy
-list1 = [[1, 2], [3, 4]]
-list2 = copy.copy(list1)
-list3 = copy.deepcopy(list1)
-
-# zmiana wartości w oryginalnej liście
-list1[0][1] = 5
-
-print(list1)  # [[1, 5], [3, 4]]
-print(list2)  # [[1, 5], [3, 4]]
-print(list3)  # [[1, 2], [3, 4]]
-
-#sortowanie listy
-arr = [8,3,4]
-arr.sort()
+arr.pop(1)
 print(arr)
-arr2 = sorted(arr)
-print(arr2)
+arr.reverse()
+print(arr)
+#print(arr.count(2))
+
+
+# #kopiowanie listy
+# import copy
+# list1 = [[1, 2], [3, 4]]
+# list2 = copy.copy(list1)
+# list3 = copy.deepcopy(list1)
+#
+# # zmiana wartości w oryginalnej liście
+# list1[0][1] = 5
+#
+# print(list1)  # [[1, 5], [3, 4]]
+# print(list2)  # [[1, 5], [3, 4]]
+# print(list3)  # [[1, 2], [3, 4]]
+#
+# #sortowanie listy
+# arr = [8,3,4]
+# arr.sort()
+# print(arr)
+# arr2 = sorted(arr)
+# print(arr2)
--- 03/list_zadania.py
+++ 03/list_zadania.py
@@ -1,4 +1,34 @@
+#4
+print("ZADANIE 4")
+num = input("Podaj parzystą listę elementów po przecinku: ")
+num_list = num.split(",")
+while len(num_list) % 2 != 0:
+    num = input(f"Podaj parzystą listę elementów po przecinku, {len(num_list)} to liczba nieparzysta: ")
+    num_list = num.split(",")
+pos_1 = num_list[int(len(num_list)/2-1)]
+pos_2 = num_list[int(len(num_list)/2)]
+if pos_1 == pos_2:
+    print(f"elementy takie same :", pos_1, pos_2)
+else:
+    print("elementy różne: ", pos_1, pos_2)
+
+
+#2
+print("ZADANIE 2")
+num = input("Podaj 10 cyfr po przecinku: ")
+num_list = num.split(",")
+while len(num_list) != 10:
+    num = input(f"Podaj 10 cyfr po przecinku NIE {len(num_list)}: ")
+    num_list = num.split(",")
+odd_num = []
+for i in range(len(num_list)-1):
+    if int(num_list[i]) % 2 != 0:
+        odd_num.append(num_list[i])
+print("Liczby nieparzyste: ", odd_num)
+
+
 #5
+print("ZADANIE 5")
 arr = [['Dorota', 'Wellman', 'dziennikarka'], ['Adam', 'Małysz', 'sportowiec'], ['Robert', 'Lewandowski', 'piłkarz'], ['Krystyna', 'Janda', 'aktorka']]
 # for i in range(len(arr)):
 #     print(arr[i][0], arr[i][1], "-", arr[i][2])
@@ -16,23 +46,23 @@ for row in arr:
             print(elem, end=' ')
     print()
 
-# arr = ["ala", "ma", "kota", "i", "psa"]
-# for id in range(5):
-#     if id == 2:
-#         print(arr[id], end="***")
-#     else:
-#         print(arr[id], end="---> ")
-#
-# for id, word in enumerate(arr):
-#     print(id, word)
-#     if id == 2:
-#         print(word, end="***")
-#     else:
-#         print(word, end="---> ")
+arr = ["ala", "ma", "kota", "i", "psa"]
+for id in range(5):
+    if id == 2:
+        print(arr[id], end="***")
+    else:
+        print(arr[id], end="---> ")
 
-# print("hello", end=" - ")
-# print("kakaka", end="****")
-# print("lalala")
+for id, word in enumerate(arr):
+    print(id, word)
+    if id == 2:
+        print(word, end="***")
+    else:
+        print(word, end="---> ")
+
+print("hello", end=" - ")
+print("kakaka", end="****")
+print("lalala")
 
 
 '''
