0b3a1726c6c364c5d72d8f7b036233d7dcab135f
RobertRze <robert_rzeszotek@sevenet.pl>
2023-05-01 23:07:55 +0200

    Tuple Tasks 1, 2

--- 03/ktotki.py
+++ 03/ktotki.py
@@ -1,4 +1,24 @@
+# #1
+print ("ZADANIE 1")
+my_tuple = ("kot", "dachowiec", "Lolek")
+animal = []
+for x in my_tuple:
+    animal.append(x)
+print(f"Mój {animal[0]} rasy {animal[1]} wabi się {animal[2]}")
+
+#2
+print ("ZADANIE 2")
+my_tuple = (1,3,4,"kot", "kot",4)
+arr = []
+for x in my_tuple:
+    count = my_tuple.count(x)
+    if count > 1:
+        arr.append(x)
+arr = list(dict.fromkeys(arr))
+print(arr)
+
 #3 Stwórz krotkę liczb całkowitych. Poproś użytkownika o podanie dowolnej liczby. Jeśli liczba znajduje się na krotce wyswietl jej indeks.
+print("ZADANIE 3")
 my_tuple = (3, 5, 3, 4, 34, 42)
 num = int(input('Podaj liczbę: '))
 if num in my_tuple:
@@ -6,19 +26,6 @@ if num in my_tuple:
     print(f"Jest {num} na krotce: ", my_tuple.index(num))
 else:
     print("NIe ma na krotce")
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
