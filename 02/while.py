6a73c0ef329b13f281d39839bd2270b2bca034c7
RobertRze <robert_rzeszotek@sevenet.pl>
2023-05-01 21:17:34 +0200

    while Task 2, Summary Tasks 2, 4, 5

--- 02/while.py
+++ 02/while.py
@@ -1,7 +1,17 @@
+#2
+print("ZADANIE 2")
+secret_num = 5
+input_num = int(input("Podaj liczbę od 1 do 20: "))
+while input_num != secret_num:
+    print("Spóbuj jeszcze raz!")
+    input_num = int(input("Podaj liczbę od 1 do 20: "))
+print("BRAWO!")
+
+#1
+print("ZADANIE 1")
 temp = 0
 while temp <= 200:
  #   print(temp)
     temp_C = round(5 / 9 * (temp - 32),2)
     print('Temp C: ', temp_C, 'Temp F: ', temp)
     temp+=20
-#C = 5/9 * (F - 32)
--- 02/zadanie.py
+++ 02/zadanie.py
@@ -1,10 +1,75 @@
-#1
-'''
-name = input('podaj imioną: ')
+#5
+print("ZADANIE 5: CIEPŁO ZIOMNO")
+import random
+r_num = random.randint(1, 100)
+for i in range(5):
+    u_num = int(input("Podaj liczbę: "))
+    num = [u_num, r_num]
+    num.sort()
+    if  num[1] - num[0] == 0:
+        print("PARZY!!!!")
+        break
+    elif num[1] - num[0] < 10:
+        print("GORĄCO")
+    elif num[1] - num[0] > 20:
+        print("ZIMNO")
+    elif num[1] - num[0] <= 20 and num[1] - num[0] >= 10 :
+        print("CIEPŁO")
+    else:
+        print("PRZEGRAŁEŚ")
+
+
+
+#4
+print('ZADANIE 4: PAPIER, KAMIEŃ, NOŻYCE')
+import random
+num_round = int(input("Podaj liczbę rund: "))
+win_u = 0
+win_c = 0
+for i in range(num_round):
+    fig_u = input("Podaj igurę kamień (k) / papier (p) / nożyce (n) / koniec (q): ")
+    list_fig_c = ["k", "p", "n"]
+    fig_c = random.choice(("k", "p", "n"))
+    if (fig_u == "k" and fig_c == "n") or (fig_u == "p" and fig_c == "k") or (fig_u == "n" and fig_c == "k"):
+        print("Wybór komputera: ", fig_c)
+        print("Wygrałeś")
+        win_u += 1
+    elif fig_u == fig_c:
+        print("Wybór komputera: ", fig_c)
+        print("REMIS")
+    elif fig_u == 'q':
+        print("Koniec gry !!!!")
+        break
+    else:
+        print("Wybór komputera: ", fig_c)
+        print("Komputer wygrał")
+        win_c += 1
+if win_u > win_c:
+    print(f"WYGRAŁEŚ CAŁĄ GRĘ. Twój wynik {win_u} Wynik komputera {win_c}")
+elif win_u == win_c:
+    print(f"REMIS W CAŁEJ GRZE. Twój wynik {win_u} Wynik komputera {win_c}")
+else:
+    print(f"KOMPUTER WYGRAŁ CAŁĄ GRĘ. Twój wynik {win_u} Wynik komputera {win_c}")
+
+
+#2
+txt = input("Enter text: ")
+x = 1
+res_txt = ''
+for i in range(len(txt)//2):
+    res_txt = res_txt + txt[x]
+    x = x + 2
+print("Way one: ", res_txt)
+
+print("Way two: ", txt[1::2])
+
+1
+name = input('podaj imioną: ')1234567
+
 name_list = name.split(',')
 for step in name_list:
     print('Hello ', step)
-'''
+
 
 #3
 lower_letters = 0
